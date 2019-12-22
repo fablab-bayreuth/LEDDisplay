c640381696c69f8525abc36017c107296f7d4e1e
holzheu <holzheu@holzheu-ThinkPad-T570>
2019-12-22 22:04:46 +0100

    0.6.1
    + fix formatation in example code of README.md

--- README.md
+++ README.md
@@ -35,21 +35,21 @@ A typical example sketch for having a text running over the display looks like t
 	const char text[] PROGMEM = "Fablab Bayreuth -- Arduino Day 2018 - 07.04.2018 -- ab 13:00";
 
 	void setup(void) {
-  		led.begin(); //initialize display (e.g. attach INT0)
-  		led.setFont(gfxFont); //set font pointer
+	  led.begin(); //initialize display (e.g. attach INT0)
+	  led.setFont(gfxFont); //set font pointer
 	}
 
 	void loop(void) {
-  		if (led.int0_flag) {
-    		if (led.wokeupFromSleep()) {
-        		led.clear(); //clear the display
-        		led.initRunning(shift_wait); //set shift_wait and set offset to zero
-    		}
-		    led.setSpeed();//calculates the speed to flash the LEDs
-    		led.runningTextPROGMEM(text);//add new char to the buffer if shift time is exceeded 
-    		led.run(); //flashes the LEDs and clears int0_flag at the end
-  		}
-  		led.sleep(); //go to sleep when there is no int0 for more than 500ms
+	  if (led.int0_flag) {
+	    if (led.wokeupFromSleep()) {
+	      led.clear(); //clear the display
+	      led.initRunning(shift_wait); //set shift_wait and set offset to zero
+	    }
+	    led.setSpeed();//calculates the speed to flash the LEDs
+	    led.runningTextPROGMEM(text);//add new char to the buffer if shift time is exceeded
+	    led.run(); //flashes the LEDs and clears int0_flag at the end
+	  }
+	  led.sleep(); //go to sleep when there is no int0 for more than 500ms
 	}
 
 
--- examples/03_LEDDisplayBasic/E_RunningText/E_RunningText.ino
+++ examples/03_LEDDisplayBasic/E_RunningText/E_RunningText.ino
@@ -1,36 +1,32 @@
-#include <LEDDisplay.h>
+include <LEDDisplay.h>
 
-//GFXfont* gfxFont = &FreeSerifBold9pt7b;
-//GFXfont* gfxFont = &FreeSerif9pt7b;
-//GFXfont* gfxFont = &FreeMonoOblique9pt7b;
+//Define a font
 GFXfont* gfxFont = &FreeMonoBoldOblique9pt7b;
 
 //Create Instance of LEDDisplay
 LEDDisplay led;
 
-
+//Set shift wait
 int shift_wait = 70; //70ms for next shift
 
 //The text to display. Text is put in PROGMEM (not in RAM).
-//This allows us to have really big test strings ~ 24kb
+//This allows us to have really big text strings ~ 24kb
 const char text[] PROGMEM = "Fablab Bayreuth -- Arduino Day 2018 - 07.04.2018 -- ab 13:00";
 
-
 void setup(void) {
-  led.begin();
-  led.setFont(gfxFont);
+  led.begin(); //initialize display (e.g. attach INT0)
+  led.setFont(gfxFont); //set font pointer
 }
 
 void loop(void) {
   if (led.int0_flag) {
     if (led.wokeupFromSleep()) {
-        led.clear();
-        led.initRunning(shift_wait);
+      led.clear(); //clear the display
+      led.initRunning(shift_wait); //set shift_wait and set offset to zero
     }
-
-    led.setSpeed();
-    led.runningTextPROGMEM(text);
-    led.run();
+    led.setSpeed();//calculates the speed to flash the LEDs
+    led.runningTextPROGMEM(text);//add new char to the buffer if shift time is exceeded
+    led.run(); //flashes the LEDs and clears int0_flag at the end
   }
-  led.sleep();
+  led.sleep(); //go to sleep when there is no int0 for more than 500ms
 }
